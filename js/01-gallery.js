94ca554d798b4927b007c36f6aa3b5fa7a152493
Piotrporadowski <122733517+Piotrporadowski@users.noreply.github.com>
2023-09-12 21:53:01 +0200

    e

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryElements = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+     <a class="gallery__link" href="${item.original}">
+       <img
+        class="gallery__image"
+         src="${item.preview}"
+         data-source="${item.original}"
+         alt="${item.description}"
+       />
+     </a>
+   </li>`;
+  })
+  .join('');
+
+galleryList.innerHTML = galleryElements;
+
+galleryList.addEventListener('click', selectImage);
+
+function selectImage(eventImage) {
+  eventImage.preventDefault();
+  if (eventImage.target.nodeName !== 'IMG') {
+    return;
+  }
+  const selectedImage = eventImage.target.dataset.source;
+  const openedImage = basicLightbox.create(`<img src="${selectedImage}">`);
+  openedImage.show();
+
+  document.addEventListener('keydown', escapePress);
+  function escapePress(eventKey) {
+    if (eventKey.code === 'Escape') {
+      openedImage.close();
+      document.removeEventListener('keydown', escapePress);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector('.gallery');
+
+const galleryElements = [];
+galleryItems.forEach(item => {
+  const element = `<li><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`;
+  galleryElements.push(element);
+});
+listGallery.innerHTML = galleryElements.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
